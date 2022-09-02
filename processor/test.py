630cb12fb8b94a9cf774c8004e10bd00e4dc3b74
Ben Hichem <77390862+benhichem@users.noreply.github.com>
2022-09-02 16:34:14 +0000

    Update test.py

--- processor/test.py
+++ processor/test.py
@@ -6,9 +6,9 @@ args = sys.argv
 print(args)
 path = args[1]
 
-distination = "output.csv"
+#distination = "path_to_file/output.csv"
 
 
 model = Model(path)
 # model.predict_to_terminal(threshold=0.99)   # 0.99 is recommended with the bigger model
-model.predict_to_csv(threshold=0.99, out_file=distination)
+model.predict_to_csv(threshold=0.99, out_file="predictions.csv")
